0f5f89645df1eb645a10611b8e3e3c22df5b1704
Amaury Schillio <aschillio@octo.com>
2017-10-16 10:41:28 +0200

    creation premiere classe MonthlyPayment et test

---
+++ src/test/java/MonthlyPaymentTest.java
@@ -0,0 +1,24 @@
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+import org.junit.Test;
+import org.mockito.Mockito;
+
+
+
+public class MonthlyPaymentTest {
+
+    @Test
+    public void name () {
+
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+        assertThat(account.getBalance()).isExactlyInstanceOf(Float.class);
+
+    }
+
+
+
+}
